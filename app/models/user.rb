27da203df8d55390e8790dc30331a82145d09ae8
ShaunApps <shaun.apps@gmail.com>
2016-03-05 21:03:02 -0500

    configured mailboxer init and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :images
 
+  acts_as_messageable
+
+  def mailboxer_username
+    self.username
+  end
+
+  def mailboxer_email(object)
+    self.email
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
+
   resources :images
   resource :users
   resource :sessions
 
   root 'welcome#index'
+
+
+
+  # mailbox folder routes
+ get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+ get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+ get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+
+ # conversations
+ resources :conversations do
+   member do
+     post :reply
+     post :trash
+     post :untrash
+   end
+ end
+
+
 
 
   get 'sessions/new'
